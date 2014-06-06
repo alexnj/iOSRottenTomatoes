960d47524346d32bb268064631822f4a0a896b8e
Alex Jose <alexnj@gmail.com>
2014-06-05 21:39:39 -0700

    Integrated CocoaPods and AFNetworking 2.2.4

---
+++ RottenTomatoesClient.h
@@ -0,0 +1,14 @@
+//
+//  RottenTomatoesClient.h
+//  iOSRottenTomatoes
+//
+//  Created by Alex on 6/5/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AFNetworking.h"
+
+@interface RottenTomatoesClient : AFHTTPRequestOperationManager
+
+@end
---
+++ RottenTomatoesClient.m
@@ -0,0 +1,13 @@
+//
+//  RottenTomatoesClient.m
+//  iOSRottenTomatoes
+//
+//  Created by Alex on 6/5/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#import "RottenTomatoesClient.h"
+
+@implementation RottenTomatoesClient
+
+@end
